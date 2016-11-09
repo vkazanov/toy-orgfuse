f964e1bf318498041e7bf6d0ea3c55a6b9ec2508
Vladimir Kazanov <vkazanov@inbox.ru>
2016-11-09 14:26:24 +0000

    Parser tokenizer

--- orgfuse.py
+++ orgfuse.py
@@ -8,8 +8,61 @@ from errno import ENOENT, EROFS
 from stat import S_IFDIR, S_IFLNK, S_IFREG
 from sys import argv, exit
 from time import time
+from StringIO import StringIO
+import re
 
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
+
+class OrgFileParser():
+
+    HEADER_TOKEN = 0
+    TEXT_TOKEN = 1
+
+    HEADER_RE = re.compile(r"(\*+) (.+)")
+
+    def __init__(self, _file):
+        self._lines = _file.readlines()
+
+    def _tokenize(self, lines):
+        tokens = []
+        cur_body_strs = []
+        for line in lines:
+            from pprint import pprint
+            print("LINE:")
+            pprint(line)
+            match = self.HEADER_RE.match(line)
+            if match is None:
+                cur_body_strs.append(line)
+            else:
+                if cur_body_strs:
+                    text_token = (self.TEXT_TOKEN, None, cur_body_strs)
+                    tokens.append(text_token)
+                    cur_body_strs = []
+                header_token = (self.HEADER_TOKEN, len(match.group(1)), match.group(2))
+                tokens.append(header_token)
+        else:
+            if cur_body_strs:
+                text_token = (self.TEXT_TOKEN, None, cur_body_strs)
+                tokens.append(text_token)
+
+        return tokens
+
+    def build_tree(self):
+        tokens = self._tokenize(self._lines)
+        from pprint import pprint
+        print("TOKENS:")
+        pprint(tokens)
+
+        root = OrgTree(OrgTree.DIR, "")
+        root.add_child(OrgTree(OrgTree.FILE, "test1"))
+        root.add_child(OrgTree(OrgTree.FILE, "test2"))
+
+        child = OrgTree(OrgTree.DIR, "test3")
+        child.add_child(OrgTree(OrgTree.FILE, "test4"))
+        child.add_child(OrgTree(OrgTree.FILE, "test5"))
+
+        root.add_child(child)
+        return root
 
 class OrgTree():
 
@@ -68,9 +121,7 @@ class OrgTreeFS(LoggingMixIn, Operations):
         return ['.', '..'] + [child for child in node.children]
 
     def getattr(self, path, fh=None):
-        print(path)
         node = self.tree.find_path(path)
-        print(path, " ->", node)
         if node is None:
             raise FuseOSError(ENOENT)
         if node.type == OrgTree.DIR:
@@ -84,15 +135,18 @@ if __name__ == '__main__':
         exit(1)
 
     logging.basicConfig(level=logging.DEBUG)
-
-    tree = OrgTree(OrgTree.DIR, "")
-    tree.add_child(OrgTree(OrgTree.FILE, "test1"))
-    tree.add_child(OrgTree(OrgTree.FILE, "test2"))
-
-    dir_child = OrgTree(OrgTree.DIR, "test3")
-    dir_child.add_child(OrgTree(OrgTree.FILE, "test4"))
-    dir_child.add_child(OrgTree(OrgTree.FILE, "test5"))
-
-    tree.add_child(dir_child)
-
-    fuse = FUSE(OrgTreeFS(tree), argv[1], foreground=True)
+    org_str = """
+* header 1
+header text
+** inner header 1
+some inner text 1
+** inner header 2
+inner text2
+** inner header 3
+*** inner inner header 1
+"""
+    print(org_str)
+    strio = StringIO(org_str)
+    parser = OrgFileParser(strio)
+    tree = parser.build_tree()
+    # fuse = FUSE(OrgTreeFS(tree), argv[1], foreground=True)
